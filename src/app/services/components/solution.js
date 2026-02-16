5c80766686eacc582852b857085b62de22ebc21a
RobertEngel98 <robert@talentsuite.io>
2026-02-16 21:18:04 +0100

    Update solution.js

--- src/app/services/components/solution.js
+++ src/app/services/components/solution.js
@@ -1,19 +1,16 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 
 const Solution = ({ title, description, items }) => {
   return (
-    <section className="solutions_section">
-      <Image src="/assets/circle.png" width={100} height={100} className="blur_image" alt="circle image" />
-
+    <section className="solutions_section" aria-label="Unsere Lösung">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-12 col-md-8 col-lg-7">
             <div className="titles text-center">
               <h2 className="text-white mb-4">{title}</h2>
               <p>{description}</p>
-              <Image src="/assets/btm-bdr.svg" width={100} height={100} className="btm_bdr" alt="Divider" />
+              <img src="/assets/btm-bdr.svg" width={100} height={4} className="btm_bdr" alt="" aria-hidden="true" />
             </div>
           </div>
         </div>
@@ -21,11 +18,11 @@ const Solution = ({ title, description, items }) => {
         <div className="row g-4 justify-content-center">
           {items.map((item, index) => (
             <div key={index} className="col-12 col-md-6 col-lg-3 col-xl-3">
-              <div className="solution_card h-100">
+              <article className="solution_card h-100">
                 <span><h3>{item.number}</h3></span>
                 <h4 className="mb-2">{item.title}</h4>
                 <p>{item.desc}</p>
-              </div>
+              </article>
             </div>
           ))}
         </div>
