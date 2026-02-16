df2a6783cdf6d980b58a6314df73a4351eafa3e1
RobertEngel98 <robert@talentsuite.io>
2026-02-16 21:17:13 +0100

    Update appointment.js

--- src/app/services/components/appointment.js
+++ src/app/services/components/appointment.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
 
-const Appointment = ({ heading, buttonText, buttonLink = "#" }) => {
+const Appointment = ({ heading, buttonText, buttonLink = "https://calendly.com/talentsuite" }) => {
   return (
-    <section className="appointment_section">
+    <section className="appointment_section" aria-label="Termin vereinbaren">
       <div className="container">
         <div className="row">
           <div className="col-12 col-md-12">
             <div className="appointment_content">
               <h2>{heading}</h2>
               <div className="hero_buttons">
-                <Link className="btns web_btns" href={buttonLink}>
+                <Link className="btns web_btns" href={buttonLink} target="_blank" rel="noopener noreferrer">
                   {buttonText}
                   <span className="btn_arrows">
                     <i className="bi bi-arrow-up-right"></i>
@@ -17,6 +17,9 @@ const Appointment = ({ heading, buttonText, buttonLink = "#" }) => {
                   </span>
                 </Link>
               </div>
+              <p style={{ fontSize: "14px", opacity: 0.5, marginTop: "16px" }}>
+                ✓ 100% kostenlos &nbsp; ✓ Unverbindlich &nbsp; ✓ Nächster Termin in 48h
+              </p>
             </div>
           </div>
         </div>
